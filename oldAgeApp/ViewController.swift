bcd58f7fd51e5dfe24a15014f49d0e3c7afd13b0
Babajide Ajayi <Babajaid@gmail.com>
2017-01-27 19:41:50 +0100

    Initial Commit

---
+++ oldAgeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oldAgeApp
+//
+//  Created by Babajide Ajayi on 1/27/17.
+//  Copyright © 2017 Babajide Ajayi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
